cc155ce4c9b6036cd402e4361a49ed80f2cbba78
ping2ravi@gmail.com <ping2ravi@gmail.com@78fc9d16-4f68-1774-8403-051c28c61cf9>
2012-11-20 20:34:36 +0000

    Sender Device ID added to ChatMessageInternalDTO

--- chataround-dto/src/main/java/com/service/chataround/dto/chat/ChatMessageInternalDto.java
+++ chataround-dto/src/main/java/com/service/chataround/dto/chat/ChatMessageInternalDto.java
@@ -7,8 +7,15 @@ public class ChatMessageInternalDto extends ChatMessageDto{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String receipientDeviceId; 
+	private String senderDeviceId; 
+	private String receipientDeviceId;
 	
+	public String getSenderDeviceId() {
+		return senderDeviceId;
+	}
+	public void setSenderDeviceId(String senderDeviceId) {
+		this.senderDeviceId = senderDeviceId;
+	}
 	public ChatMessageInternalDto(ChatMessageDto chatMessageDto){
 		super(chatMessageDto);
 	}
