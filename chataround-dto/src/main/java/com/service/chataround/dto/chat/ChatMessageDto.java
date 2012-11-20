9bf689564a3f3ee08b415b989e4127170ed81b01
luislukas@gmail.com <luislukas@gmail.com@78fc9d16-4f68-1774-8403-051c28c61cf9>
2012-11-20 20:14:19 +0000

    Changes to override cause andorid project cannot pickup the override annotation plus needed fields

--- chataround-dto/src/main/java/com/service/chataround/dto/chat/ChatMessageDto.java
+++ chataround-dto/src/main/java/com/service/chataround/dto/chat/ChatMessageDto.java
@@ -1,6 +1,7 @@
 package com.service.chataround.dto.chat;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class ChatMessageDto implements Serializable{
 
@@ -8,11 +9,16 @@ public class ChatMessageDto implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private Long id ;
 	private String senderId;
 	private String recipientId;
 	private String message;
 	private String appId;
 	private String nickName;
+	private boolean mine;
+	private boolean sent;
+	private Date time;
+	
 	
 	public ChatMessageDto(ChatMessageDto chatMessageDto) {
 		super();
@@ -58,6 +64,38 @@ public class ChatMessageDto implements Serializable{
 
 	public void setNickName(String nickName) {
 		this.nickName = nickName;
+	}
+
+	public boolean isMine() {
+		return mine;
+	}
+
+	public void setMine(boolean mine) {
+		this.mine = mine;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public boolean isSent() {
+		return sent;
+	}
+
+	public void setSent(boolean sent) {
+		this.sent = sent;
 	}
 
 }
--- chataround-dto/src/main/java/com/service/chataround/dto/register/RegisterUserRequestDto.java
+++ chataround-dto/src/main/java/com/service/chataround/dto/register/RegisterUserRequestDto.java
@@ -8,22 +8,18 @@ public class RegisterUserRequestDto extends UserDto implements HasError{
 	private static final long serialVersionUID = 1L;
 	private String serverMessage;
 	private String responseStatus;
-	@Override
 	public String getServerMessage() {
 		return serverMessage;
 	}
 
-	@Override
 	public void setServerMessage(String serverMessage) {
 		this.serverMessage = serverMessage;
 	}
 
-	@Override
 	public String getResponseStatus() {
 		return responseStatus;
 	}
 
-	@Override
 	public void setResponseStatus(String responseStatus) {
 		this.responseStatus = responseStatus;
 	}
