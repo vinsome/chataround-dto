3b59bceec9af932f806c7b26a14654d9f2d3e0bc
ping2ravi@gmail.com <ping2ravi@gmail.com@78fc9d16-4f68-1774-8403-051c28c61cf9>
2012-11-22 18:11:08 +0000

    Removed enum setter and getters

--- chataround-dto/src/main/java/com/service/chataround/dto/register/RegisterUserRequestDto.java
+++ chataround-dto/src/main/java/com/service/chataround/dto/register/RegisterUserRequestDto.java
@@ -2,9 +2,11 @@ package com.service.chataround.dto.register;
 
 import java.io.Serializable;
 
+import com.service.chataround.dto.HasError;
 import com.service.chataround.dto.UserWebEntity;
+import com.service.chataround.dto.chat.UserDto;
 
-public class RegisterUserRequestDto implements Serializable,UserWebEntity{
+public class RegisterUserRequestDto extends UserDto implements Serializable,HasError{
 	
 	/*
 	 * 
@@ -12,19 +14,6 @@ public class RegisterUserRequestDto implements Serializable,UserWebEntity{
 	private static final long serialVersionUID = 1L;
 	private String serverMessage;
 	private String responseStatus;
-	private String email;
-	private String password;
-	private String nickName;
-	private Double longitude;
-	private Double lattitude;
-	private String statusMessage;
-	private String deviceId;
-	private String userId;
-	private String gender;
-	private String smallImageUrl;
-	private String mediumImageUrl;
-	private String largeImageUrl;
-
 	
 	public RegisterUserRequestDto(){
 		
@@ -43,81 +32,5 @@ public class RegisterUserRequestDto implements Serializable,UserWebEntity{
 
 	public void setResponseStatus(String responseStatus) {
 		this.responseStatus = responseStatus;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getNickName() {
-		return nickName;
-	}
-	public void setNickName(String nickName) {
-		this.nickName = nickName;
-	}
-	public Double getLongitude() {
-		return longitude;
-	}
-	public void setLongitude(Double longitude) {
-		this.longitude = longitude;
-	}
-	public Double getLattitude() {
-		return lattitude;
-	}
-	public void setLattitude(Double lattitude) {
-		this.lattitude = lattitude;
-	}
-	public String getStatusMessage() {
-		return statusMessage;
-	}
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
-	}
-	public String getDeviceId() {
-		return deviceId;
-	}
-	public void setDeviceId(String deviceId) {
-		this.deviceId = deviceId;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		Gender gendreEnum = Gender.parse(gender);
-		if(gendreEnum == null){
-			throw new RuntimeException(gender + " is not a correct value for Gender");
-		}
-		this.gender = gender;
-	}
-	public String getSmallImageUrl() {
-		return smallImageUrl;
-	}
-	public void setSmallImageUrl(String smallImageUrl) {
-		this.smallImageUrl = smallImageUrl;
-	}
-	public String getMediumImageUrl() {
-		return mediumImageUrl;
-	}
-	public void setMediumImageUrl(String mediumImageUrl) {
-		this.mediumImageUrl = mediumImageUrl;
-	}
-	public String getLargeImageUrl() {
-		return largeImageUrl;
-	}
-	public void setLargeImageUrl(String largeImageUrl) {
-		this.largeImageUrl = largeImageUrl;
 	}
 }
